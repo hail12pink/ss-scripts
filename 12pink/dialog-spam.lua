8257bfd78295789e1cb6f9cdea720bc3568d6b2c
hail12pink <59028187+hail12pink@users.noreply.github.com>
2020-12-08 16:12:14 -0500

    Update dialog-spam.lua

--- 12pink/dialog-spam.lua
+++ 12pink/dialog-spam.lua
@@ -1,7 +1,7 @@
 while wait() do
   for _, v in pairs(workspace:GetDescendants()) do
     coroutine.resume(coroutine.create(function()
-          if not v["Dialog"] then
+          if not v:FindFirstChildOfClass("Dialog") then
             local d = Instance.new("Dialog", v)
             d.Tone = math.random(0, 2)
             d.Purpose = math.random(0, 2)
@@ -12,6 +12,6 @@ while wait() do
                 d.Purpose = math.random(0, 2)
             end)
           end
-    end))
+        end))
   end
 end
