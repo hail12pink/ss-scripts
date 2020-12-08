410f10e0bca5fde4ea60d9fee12f9fd063a216ad
hail12pink <59028187+hail12pink@users.noreply.github.com>
2020-12-08 16:01:28 -0500

    Update dialog-spam.lua

--- 12pink/dialog-spam.lua
+++ 12pink/dialog-spam.lua
@@ -4,7 +4,8 @@ while wait() do
           local d = Instance.new("Dialog", v)
           d.Tone = math.random(0, 2)
           d.Purpose = math.random(0, 2)
-          d.TriggerDistance = 0
+          d.TriggerDistance = 0.01
+          d.ConversationDistance = 0.01
     end))
   end
 end
