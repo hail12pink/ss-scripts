191e0be53d6fdcb982833090f5c77026cfed6476
hail12pink <59028187+hail12pink@users.noreply.github.com>
2020-12-08 16:05:34 -0500

    Update dialog-spam.lua

--- 12pink/dialog-spam.lua
+++ 12pink/dialog-spam.lua
@@ -1,15 +1,17 @@
 while wait() do
   for _, v in pairs(workspace:GetDescendants()) do
     coroutine.resume(coroutine.create(function()
-          local d = Instance.new("Dialog", v)
-          d.Tone = math.random(0, 2)
-          d.Purpose = math.random(0, 2)
-          d.TriggerDistance = 0.01
-          d.ConversationDistance = 0.01
-          spawn(function()
-              d.Tone = math.random(0, 2)
-              d.Purpose = math.random(0, 2)
-          end)
+          if not v["Dialog"] then
+            local d = Instance.new("Dialog", v)
+            d.Tone = math.random(0, 2)
+            d.Purpose = math.random(0, 2)
+            d.TriggerDistance = 0.01
+            d.ConversationDistance = 0.01
+            spawn(function()
+                d.Tone = math.random(0, 2)
+                d.Purpose = math.random(0, 2)
+            end)
+          end
     end))
   end
 end
