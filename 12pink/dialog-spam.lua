0fe55602839f05a96d502990c7806ddf2e644714
hail12pink <59028187+hail12pink@users.noreply.github.com>
2020-12-08 14:59:44 -0500

    Create dialog-spam.lua

---
+++ 12pink/dialog-spam.lua
@@ -0,0 +1,10 @@
+while wait() do
+  for _, v in pairs(workspace:GetDescendants()) do
+    coroutine.resume(coroutine.create(function()
+          local d = Instance.new("Dialog", v)
+          d.Tone = math.random(0, 2)
+          d.Purpose = math.random(0, 2)
+          d.TriggerDistance = 0
+    end))
+  end
+end
